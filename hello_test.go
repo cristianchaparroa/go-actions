d39f347741e40f79abcbef5a6cb97a557db2e732
Cristian  Chaparro <cristianchaparroa@gmail.com>
2020-04-20 15:39:18 -0500

    ci: testing gactions

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestGreetsGitHub(t *testing.T) {
-    result := Greet()
-    if result != "Hello GitHub Actions" {
-        t.Errorf("Greet() = %s; want Hello GitHub actions", result)
-    }
+	result := Greet()
+	if result != "Hello GitHub Actions. Dev.to is awesome" {
+		t.Errorf("Greet() = %s; want ello GitHub Actions. Dev.to is awesome", result)
+	}
 }
